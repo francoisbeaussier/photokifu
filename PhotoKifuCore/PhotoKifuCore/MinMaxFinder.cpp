59bca4a3876a8e905aea07688674baea4444e8bc
Francois <github@beaussier.net>
2013-09-15 11:08:59 +1000

    Removed unused code.

--- PhotoKifuCore/PhotoKifuCore/MinMaxFinder.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MinMaxFinder.cpp
-//  PhotoKifuCore
-//
-//  Created by Francois Beaussier on 13/06/13.
-//  Copyright (c) 2013 Francois Beaussier. All rights reserved.
-//
-
-#include "MinMaxFinder.h"
